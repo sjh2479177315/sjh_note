7a9a7f760e24cc1d1bd9d74e338cf0118c80ed4f
sunjiahui <sunjiahui@sunjiahuideMacBook-Pro-2.local>
2023-03-27 14:58:45 +0800

    add

---
+++ all_study_note/src/main/java/com/sjh/note/all_study_note/AllStudyNoteApplication.java
@@ -0,0 +1,13 @@
+package com.sjh.note.all_study_note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllStudyNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllStudyNoteApplication.class, args);
+    }
+
+}
---
+++ all_study_note/src/test/java/com/sjh/note/all_study_note/AllStudyNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjh.note.all_study_note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllStudyNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
